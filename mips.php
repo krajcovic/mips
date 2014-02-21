099079f014a2d35bc5c042e0ef941aef3e9db3b1
Krajcovic <dusan.krajcovic@gmail.com>
2014-02-21 14:01:18 +0100

    Logo for mips module.

--- mips.php
+++ mips.php
@@ -1,29 +1,35 @@
 
 <?php
+
 if (!defined('_PS_VERSION_'))
-  exit;
- 
-class Mips extends Module
-  {
-  public function __construct()
-    {
-    $this->name = 'mips';
-    $this->tab = 'Test';
-    $this->version = 1.0;
-    $this->author = 'Dusan Krajcovic';
-    $this->need_instance = 0;
- 
-    parent::__construct();
- 
-    $this->displayName = $this->l('Mips module');
-    $this->description = $this->l('Module for pay on MIPS.');
+    exit;
+
+class Mips extends Module {
+
+    public function __construct() {
+        $this->name = 'mips';
+        $this->tab = 'Test';
+        $this->version = 1.0;
+        $this->author = 'Dusan Krajcovic';
+        $this->need_instance = 0;
+
+        parent::__construct();
+
+        $this->displayName = $this->l('Mips module');
+        $this->description = $this->l('Module for pay on MIPS.');
     }
- 
-  public function install()
-    {
-    if (parent::install() == false)
-      return false;
-    return true;
+
+    public function install() {
+        if (parent::install() == false)
+            return false;
+        return true;
     }
-  }
+
+    public function uninstall() {
+        if (!parent::uninstall())
+            Db::getInstance()->Execute('DELETE FROM `' . _DB_PREFIX_ . 'mips`');
+        parent::uninstall();
+    }
+
+}
 ?>
